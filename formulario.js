6b46436697dd5d1f637750b484d682ef253b4619
Luiz <luizfelipilemo@gmail.com>
2024-10-14 21:28:01 -0300

    deu certo sapoha?

---
+++ formulario.js
@@ -0,0 +1,16 @@
+function CapturarValor(){
+    var nome = document.getElementById('nome').value
+    var  = document.getElementById('nome').value
+    var nome = document.getElementById('nome').value
+    var nome = document.getElementById('nome').value
+    document.getElementById('NomeColocarValor').value = nome
+    document.getElementById('NomeColocarValor').value = nome
+    document.getElementById('NomeColocarValor').value = nome
+    document.getElementById('NomeColocarValor').value = nome
+
+    LimparCampos()
+}
+
+function Limparcampos(){
+    document.getElementById('nome').value=''
+}
